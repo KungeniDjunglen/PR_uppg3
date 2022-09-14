7fe541f662b9dcaa5c58daf1a2c1bfc739e40ceb
KungeniDjungeln <thhe05001@utb.vaxjo.se>
2022-09-14 12:09:58 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uppgift2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tack för att du valde att hyra bil hoss oss.");
+            Console.WriteLine("Hur många dagar hade ni tänkt att hyra bilen? ");
+            string Days = Console.ReadLine();
+            Console.WriteLine("Hur långt hade ni tänkt att köra med bilen? ");
+            string Distance = Console.ReadLine();
+            int iDays = int.Parse(Days);
+            int iDistace = int.Parse(Distance);
+            int Cost = 300 + iDays * 500 + iDistace;
+            Console.WriteLine("er kostnad är: " + Cost);
+
+        }
+    }
+}
